03aa2b81bd7904d81f69cc2e57561e14c9f8d7db
Dmitrij Koniajev <dimchansky@gmail.com>
2022-12-21 00:59:01 +0200

    no characters in prefix are considered magic in CompareString: works with Ka-50_3 now

--- scripts/DCSHMD/Util.lua
+++ scripts/DCSHMD/Util.lua
@@ -1,7 +1,7 @@
 DCSHMD_Util = {}
 
 function DCSHMD_Util.CompareString(str1, str2)
-    if str1 == str2 or string.find(str1, str2) ~= nil then
+    if str1 == str2 or string.find(str1, str2, 1, true) ~= nil then
         return true
     end
 
